5976c032b20771e4750fb13b991166e15209d928
anas-ecam2020 <20316@ecam.be>
2021-04-28 11:59:37 +0200

    Create GET method for recipes within service

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// Créer une interface Recipe 
+export interface Recipe {
+  id: number,
+  category_id: number,
+  title: string,
+  content: string,
+  created_at: Date,
+  image: string,
+  favorite: number,
+  time: number,
+  difficulty: string,
+  portions: number
+}
+
+const endpoint = "127.0.0.1:8000/api/";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class RestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  // Observable permet d'avoir une gestion asynchrone d'une requête
+  // Obtenir résultat requête GET pour toutes les recettes
+  getProducts(): Observable<any> {
+  return this.http.get<Recipe>(endpoint + 'recipes');
+  }
 }
